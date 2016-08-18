7aa8f1ecb24adbc8f472230b26d2dc6f562802d4
sakamura <michel@micheldonais.com>
2016-08-18 13:52:03 -0400

    Initial Commit

---
+++ StyleSheet/StyleSheet.h
@@ -0,0 +1,13 @@
+//
+//  StyleSheet.h
+//  StyleSheet
+//
+//  Created by Michel Donais on 16-08-18.
+//  Copyright © 2016 Michel Donais. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StyleSheet : NSObject
+
+@end
